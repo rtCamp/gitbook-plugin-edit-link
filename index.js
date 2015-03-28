bc29d8e7789cfead593795e78536c150455a7db5
Anna Warzecha <anna.warzecha@gmail.com>
2015-03-28 23:58:50 +0100

    Add support for multilingual gitbooks

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ module.exports = {
                 config.label = "Edit This Page";
             }
 
-            rtEditLink = '<a href="' + config.base + '/' + page.path + '" class="btn fa fa-edit pull-left">&nbsp;&nbsp;' + config.label + '</a>';
+            // We need to calculate the path this way because multilingual
+            // books have wrong page.path
+            path = "/" + page.path;
+            if(this.options.originalInput !== undefined) {
+                path = page.rawPath.replace(this.options.originalInput, '');
+            }
+
+            rtEditLink = '<a href="' + config.base + path + '" class="btn fa fa-edit pull-left">&nbsp;&nbsp;' + config.label + '</a>';
 
             page.content = page.content.replace (
                 '<!-- Actions Right -->',
